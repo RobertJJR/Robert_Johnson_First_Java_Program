20ddb41d9513451af86f5f6fd252e5d1dd6921da
Robert Johnson Jr <50351795+RobertJJR@users.noreply.github.com>
2019-06-13 22:32:01 -0500

    Add files via upload

---
+++ Robert_Johnson_First_Java_Program/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Robert Johnson");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Grand Prairie, TX");
+    }
+}
